159c619aaa15e1b656e1a6909624e9d17feccb49
DengYun <tdzl2003@gmail.com>
2015-12-20 22:35:37 +0800

    Re-implement with promisify

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 import { DeviceEventEmitter, NativeModules } from 'react-native';
-
-let registered = false;
+import promisify from 'es6-promisify';
 const WeChat = NativeModules.WeChat;
 
 // Translate a pure object or message into a Error instance.
-function translateError(err) {
-  if (typeof(err) == 'object' && !(err instanceof Error)) {
-    const ret = new Error(err.message);
-    ret.errCode = err.errCode || err.err;
-  } else if (typeof(err) == 'string') {
-    throw new Error(err);
-  } else {
-    throw err;
+function translateError(err, result) {
+  if (!err) {
+    this.resolve(result);
   }
+  if (typeof err === 'object') {
+    if (err instanceof Error) {
+      return this.reject(ret);
+    }
+    return this.reject(Object.assign(new Error(err.message), { errCode: err.errCode }));
+  } else if (typeof(err) === 'string') {
+    return this.reject(new Error(err));
+  }
+  this.reject(Object.assign(new Error(), { origin: err }));
 }
 
 // Save callback and wait for future event.
@@ -20,183 +23,45 @@ const authCallbackList = {};
 function waitForAuthResponse(state) {
   return new Promise((resolve, reject) => {
     authCallbackList[state] = result => {
-      if (result.errCode != 0) {
-        // Failed.
+      if (result.errCode !== 0) {
         const err = new Error(result.errMsg);
-        err.code = result.errCode;
+        err.errCode = result.errCode;
         reject(err);
       } else {
         resolve(result);
       }
-    }
-  }).catch(translateError);;
+    };
+  });
 }
-function onWeChatResponse(resp) {
-  if (resp.type == 'SendAuth.Resp') {
+
+DeviceEventEmitter.addListener('WeChat_Resp', resp => {
+  if (resp.type === 'SendAuth.Resp') {
     const callback = authCallbackList[resp.state];
     delete authCallbackList[resp.state];
     return callback && callback(resp);
   }
-}
-DeviceEventEmitter.addListener('WeChat_Resp', onWeChatResponse);
+  if (__DEV__) {
+    throw new Error('Unsupported response type: ' + resp.type);
+  }
+});
 
-function assertSuccessed(result) {
-  if (!result) {
-    throw new Error("WeChat API returns false.(failed)");
-  }
-}
+export const registerApp = promisify(WeChat.registerApp, translateError);
 
-export function registerApp(appid, callback) {
-  const ret =  new Promise((resolve, reject) => {
-    if (registered){
-      return reject(new Error("This app is already registered."));
-    }
-    registered = true;
-    WeChat.registerApp(appid, resolve)
-  }).then(assertSuccessed);
-  if (callback){
-    ret.then(result=>callback(result), err=>callback(false));
-  }
-  return ret;
-}
+export const registerAppWithDescription = promisify(WeChat.registerAppWithDescription, translateError);
 
-export function registerAppWithDescription(appid, appdesc, callback) {
-  const ret = new Promise((resolve, reject) => {
-    if (registered){
-      return reject(new Error("This app is already registered."));
-    }
-    WeChat.registerAppWithDescription(appid, appdesc, resolve)
-  }).then(assertSuccessed);
-  if (callback){
-    ret.then(result=>callback(result), err=>callback(false));
-  }
-  return ret;
-}
+export const isWXAppInstalled = promisify(WeChat.isWXAppInstalled, translateError);
 
-export function isWXAppinstalled(callback) {
-  const ret = new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    WeChat.isWXAppinstalled(resolve);
-  });
-  if (callback){
-    ret.then(result=>callback(result), err=>callback(false));
-  }
-  return ret;
-}
+export const isWXAppSupportApi = promisify(WeChat.isWXAppSupportApi, translateError);
 
-export function isWXAppSupportApi(callback = assertSuccessed) {
-  const ret =  new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    WeChat.isWXAppSupportApi(resolve);
-  });
-  if (callback){
-    ret.then(result=>callback(result), err=>callback(false));
-  }
-  return ret;
-}
+export const getApiVersion = promisify(WeChat.getApiVersion, translateError);
 
-export function getApiVersion(callback) {
-  const ret = new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    WeChat.getApiVersion((err, result)=>{
-      err ? reject(err) : resolve(result);
-    });
-  }).catch(translateError);
-  if (callback){
-    ret.then(result=>callback(null, result), callback);
-  }
-  return ret;
-}
+export const openWXApp = promisify(WeChat.openWXApp, translateError);
 
-export function openWXApp(callback = assertSuccessed) {
-  const ret = new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    WeChat.openWXApp(resolve);
-  })
-  if (callback){
-    ret.then(result=>callback(result), err=>callback(false));
-  }
-  return ret;
-}
+export const nativeSendAuthRequest = promisify(WeChat.sendAuthRequest, translateError);
 
-export function sendAuthRequest(state, callback) {
-  if (typeof(state) == 'function') {
-    callback = state;
-    state = undefined;
-  }
-  if (!state) {
-    // Generate a random, unique state.
-    state = Math.random().toString(16).substr(2) + "_" + new Date().getTime();
-  }
-  const ret = new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    if (authCallbackList[state]) {
-      throw new Error('Last request of state `' + state + '` is not responsed yet.');
-    }
-    WeChat.sendAuthRequest(state, resolve);
-  }).then(assertSuccessed)
-    .then(()=>waitForAuthResponse(state));
-  if (callback){
-    ret.then(result=>callback(null, result), callback);
-  }
-  return ret;
-}
-
-export function sendRequest(openid, callback){
-  const ret = new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    WeChat.sendRequest(openid, resolve);
-  }).then(assertSuccessed);
-  if (callback){
-    ret.then(result=>callback(null, result), callback);
-  }
-  return ret;
-}
-
-export function sendSuccessResponse(callback){
-  const ret = new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    WeChat.sendSuccessResponse(resolve);
-  }).then(assertSuccessed);
-  if (callback){
-    ret.then(result=>callback(null, result), callback);
-  }
-  return ret;
-}
-
-export function sendErrorCommonResponse(message, callback){
-  const ret = new Promise((resolve, reject) => {
-    if (!registered){
-      return reject(new Error("Must call registerApp() first."));
-    }
-    WeChat.sendErrorCommonResponse(message, resolve);
-  }).then(assertSuccessed);
-  if (callback){
-    ret.then(result=>callback(null, result), callback);
-  }
-  return ret;
-}
-
-export function sendErrorUserCancelResponse(message, callback){
-  const ret = new Promise((resolve, reject) => {
-    WeChat.sendErrorUserCancelResponse(message, resolve);
-  }).then(assertSuccessed);
-  if (callback){
-    ret.then(result=>callback(null, result), callback);
-  }
-  return ret;
+export function sendAuthRequest(scopes, state) {
+  // Generate a random, unique state if not provided.
+  const _state = state || Math.random().toString(16).substr(2) + '_' + new Date().getTime();
+  return nativeSendAuthRequest(scopes || 'snsapi_userinfo', _state)
+    .then(() => waitForAuthResponse(state, resolve));
 }
