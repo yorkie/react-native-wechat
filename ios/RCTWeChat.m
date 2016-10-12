8b3659fe04dc5c5322a4532745dc4457f0e60797
yorkie <yorkiefixer@gmail.com>
2016-10-13 07:26:10 +0800

    ios: fix compatible with v0.35

--- ios/RCTWeChat.m
+++ ios/RCTWeChat.m
@@ -289,7 +289,8 @@ RCT_EXPORT_METHOD(pay:(NSDictionary *)data
     if (imageUrl.length && _bridge.imageLoader) {
         NSURL *url = [NSURL URLWithString:imageUrl];
         NSURLRequest *imageRequest = [NSURLRequest requestWithURL:url];
-        [_bridge.imageLoader loadImageWithURLRequest:imageRequest size:CGSizeMake(100, 100) scale:1 clipped:FALSE resizeMode:RCTResizeModeStretch progressBlock:nil completionBlock:^(NSError *error, UIImage *image) {
+        [_bridge.imageLoader loadImageWithURLRequest:imageRequest size:CGSizeMake(100, 100) scale:1 clipped:FALSE resizeMode:RCTResizeModeStretch progressBlock:nil partialLoadBlock:nil
+            completionBlock:^(NSError *error, UIImage *image) {
             [self shareToWeixinWithData:aData thumbImage:image scene:aScene callBack:aCallBack];
         }];
     } else {
