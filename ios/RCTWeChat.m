b4f261f8d81a90c57025b8ceb4f5a07736b8f21b
Johnny iDay <iDay@MacBook-Pro.local>
2017-05-20 21:53:48 +0800

    modified:   android/src/main/java/com/theweflex/react/WeChatModule.java 	modified:   ios/RCTWeChat.m 	modified:   ios/WXApi.h 	modified:   ios/WXApiObject.h 	modified:   ios/WechatAuthSDK.h 	modified:   ios/libWeChatSDK.a

--- ios/RCTWeChat.m
+++ ios/RCTWeChat.m
@@ -66,7 +66,7 @@ RCT_EXPORT_METHOD(registerAppWithDescription:(NSString *)appid
                   :(NSString *)appdesc
                   :(RCTResponseSenderBlock)callback)
 {
-    callback(@[[WXApi registerApp:appid withDescription:appdesc] ? [NSNull null] : INVOKE_FAILED]);
+    callback(@[[WXApi registerApp:appid] ? [NSNull null] : INVOKE_FAILED]);
 }
 
 RCT_EXPORT_METHOD(isWXAppInstalled:(RCTResponseSenderBlock)callback)
@@ -154,10 +154,10 @@ RCT_EXPORT_METHOD(pay:(NSDictionary *)data
                   :(RCTResponseSenderBlock)callback)
 {
     PayReq* req             = [PayReq new];
-    req.partnerId           = data[@"partnerId"];
-    req.prepayId            = data[@"prepayId"];
-    req.nonceStr            = data[@"nonceStr"];
-    req.timeStamp           = [data[@"timeStamp"] unsignedIntValue];
+    req.partnerId           = data[@"partnerid"];
+    req.prepayId            = data[@"prepayid"];
+    req.nonceStr            = data[@"noncestr"];
+    req.timeStamp           = [data[@"timestamp"] unsignedIntValue];
     req.package             = data[@"package"];
     req.sign                = data[@"sign"];
     BOOL success = [WXApi sendReq:req];
