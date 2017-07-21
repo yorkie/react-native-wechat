3377b10d5e1854ff420657eaa081bf3225edac66
Bell <bell@greedlab.com>
2017-07-21 18:47:32 +0800

    add thumb when share image

--- android/src/main/java/com/theweflex/react/WeChatModule.java
+++ android/src/main/java/com/theweflex/react/WeChatModule.java
@@ -249,16 +249,12 @@ public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEv
                     getExtension(imageUrl, new ExtensionCallback() {
                         @Override
                         public void invoke(@Nullable String extension) {
-                            if (extension != null && extension.equals("gif")) { // gif 图片不传缩略图会分享不出去
-                                getImage(finalThumbUri, new ResizeOptions(100, 100), new ImageCallback() {
-                                    @Override
-                                    public void invoke(@Nullable Bitmap bitmap) {
-                                        _share(scene, data, bitmap, callback);
-                                    }
-                                });
-                            } else { // 普通图片传缩略图会只分享缩略图
-                                _share(scene, data, null, callback);
-                            }
+                            getImage(finalThumbUri, new ResizeOptions(100, 100), new ImageCallback() {
+                                @Override
+                                public void invoke(@Nullable Bitmap bitmap) {
+                                    _share(scene, data, bitmap, callback);
+                                }
+                            });
                         }
                     });
                 }
