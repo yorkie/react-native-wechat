f7a428ff16fc05ec3e3ed2c2804e967d124c0971
Bell <bell@greedlab.com>
2017-08-21 10:56:41 +0800

    fix: NullPointerException
    ref: http://crashes.to/s/1a6701b08ad

--- android/src/main/java/com/theweflex/react/WeChatModule.java
+++ android/src/main/java/com/theweflex/react/WeChatModule.java
@@ -112,7 +112,9 @@ public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEv
 
     public static void handleIntent(Intent intent) {
         for (WeChatModule mod : modules) {
-            mod.api.handleIntent(intent, mod);
+            if (mod.api != null) {
+                mod.api.handleIntent(intent, mod);
+            }
         }
     }
 
