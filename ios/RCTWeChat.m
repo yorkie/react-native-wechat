46ec17e40821d267ef4a9165e46e9e53343e5c81
messense <messense@icloud.com>
2017-10-30 14:41:36 +0800

    Explicitly set requiresMainQueueSetup (#307)

--- ios/RCTWeChat.m
+++ ios/RCTWeChat.m
@@ -55,6 +55,10 @@ RCT_EXPORT_MODULE()
     return dispatch_get_main_queue();
 }
 
++ (BOOL)requiresMainQueueSetup {
+    return YES;
+}
+
 RCT_EXPORT_METHOD(registerApp:(NSString *)appid
                   :(RCTResponseSenderBlock)callback)
 {
