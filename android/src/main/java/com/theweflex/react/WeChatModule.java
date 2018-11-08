7e370c1397b86e65d0824bef43c3ae7e0e76548f
aijason <977854695@qq.com>
2018-11-08 14:40:16 +0800

    fix big picture sharing failure problem on Android
    修复在安卓上大图分享失败问题

--- android/src/main/java/com/theweflex/react/WeChatModule.java
+++ android/src/main/java/com/theweflex/react/WeChatModule.java
@@ -3,6 +3,7 @@ package com.theweflex.react;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Matrix;
 import android.net.Uri;
 import android.support.annotation.Nullable;
 
@@ -172,7 +173,7 @@ public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEv
         }
         _share(SendMessageToWX.Req.WXSceneSession, data, callback);
     }
-    
+
     @ReactMethod
     public void shareToFavorite(ReadableMap data, Callback callback) {
         if (api == null) {
@@ -230,7 +231,8 @@ public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEv
             this._getImage(uri, new ResizeOptions(100, 100), new ImageCallback() {
                 @Override
                 public void invoke(@Nullable Bitmap bitmap) {
-                    WeChatModule.this._share(scene, data, bitmap, callback);
+                    Bitmap bitmapThumbnail = createBitmapThumbnail(bitmap);
+                    WeChatModule.this._share(scene, data, bitmapThumbnail, callback);
                 }
             });
         } else {
@@ -509,4 +511,22 @@ public class WeChatModule extends ReactContextBaseJavaModule implements IWXAPIEv
         void invoke(@Nullable WXMediaMessage.IMediaObject mediaObject);
     }
 
+    //图片压缩
+    public Bitmap createBitmapThumbnail(Bitmap bitMap) {
+        int width = bitMap.getWidth();
+        int height = bitMap.getHeight();
+        // 设置想要的大小
+        int newWidth = 99;
+        int newHeight = 99;
+        // 计算缩放比例
+        float scaleWidth = ((float) newWidth) / width;
+        float scaleHeight = ((float) newHeight) / height;
+        // 取得想要缩放的matrix参数
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        // 得到新的图片
+        Bitmap newBitMap = Bitmap.createBitmap(bitMap, 0, 0, width, height,
+                matrix, true);
+        return newBitMap;
+    }
 }
