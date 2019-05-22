7c707c6d2dae8556cef17c99cc2c820630716889
Zhigang Fang <zhigang1992@gmail.com>
2019-05-22 10:28:17 +0800

    chore: add typescript definitions (#455)

---
+++ index.d.ts
@@ -0,0 +1,60 @@
+declare module "react-native-wechat" {
+  export function registerApp(appId: string): Promise<boolean>;
+  export function registerAppWithDescription(
+    appId: string,
+    desc: string
+  ): Promise<boolean>;
+  export function isWXAppInstalled(): Promise<boolean>;
+  export function isWXAppSupportApi(): Promise<boolean>;
+  export function getApiVersion(): Promise<string>;
+  export function openWXApp(): Promise<boolean>;
+  export interface AuthResponse {
+    errCode?: number;
+    errStr?: string;
+    openId?: string;
+    code?: string;
+    url?: string;
+    lang?: string;
+    country?: string;
+  }
+  export function sendAuthRequest(
+    scope: string | string[],
+    state?: string
+  ): Promise<AuthResponse>;
+  export interface ShareMetadata {
+    type:
+      | "news"
+      | "text"
+      | "imageUrl"
+      | "imageFile"
+      | "imageResource"
+      | "video"
+      | "audio"
+      | "file";
+    thumbImage?: string;
+    description?: string;
+    webpageUrl?: string;
+    imageUrl?: string;
+    videoUrl?: string;
+    musicUrl?: string;
+    filePath?: string;
+    fileExtension?: string;
+  }
+  export function shareToTimeline(
+    message: ShareMetadata
+  ): Promise<{ errCode?: number; errStr?: string }>;
+  export function shareToSession(
+    message: ShareMetadata
+  ): Promise<{ errCode?: number; errStr?: string }>;
+  export interface PaymentLoad {
+    partnerId: string;
+    prepayId: string;
+    nonceStr: string;
+    timeStamp: string;
+    package: string;
+    sign: string;
+  }
+  export function pay(
+    payload: PaymentLoad
+  ): Promise<{ errCode?: number; errStr?: string }>;
+}
